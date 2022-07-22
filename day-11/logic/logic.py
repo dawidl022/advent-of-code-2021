d72f6913889bd223a1acf202e04453501b10d9d0
Dawid Lachowicz <domdaw7@gmail.com>
2022-07-22 07:26:01 +0100

    Refactor day 11 in Python

--- day-11/logic/logic.py
+++ day-11/logic/logic.py
@@ -4,20 +4,7 @@ MAX_ENERGY = 9
 def increment_energy_levels(octos):
     for i in range(len(octos)):
         for j in range(len(octos[i])):
-            octos[i][j] += 1
-            if octos[i][j] == MAX_ENERGY + 1:
-                energise_surrounding(octos, i, j)
-
-
-def energise_surrounding(octos: list[list[int]], i: int, j: int):
-    energise_octo(octos, i - 1, j - 1)
-    energise_octo(octos, i - 1, j)
-    energise_octo(octos, i - 1, j + 1)
-    energise_octo(octos, i, j - 1)
-    energise_octo(octos, i, j + 1)
-    energise_octo(octos, i + 1, j - 1)
-    energise_octo(octos, i + 1, j)
-    energise_octo(octos, i + 1, j + 1)
+            energise_octo(octos, i, j)
 
 
 def energise_octo(octos: list[list[int]], i: int, j: int):
@@ -31,3 +18,14 @@ def energise_octo(octos: list[list[int]], i: int, j: int):
 
 def out_of_bounds(octos, i, j):
     return i < 0 or i >= len(octos) or j < 0 or j >= len(octos[i])
+
+
+def energise_surrounding(octos: list[list[int]], i: int, j: int):
+    energise_octo(octos, i - 1, j - 1)
+    energise_octo(octos, i - 1, j)
+    energise_octo(octos, i - 1, j + 1)
+    energise_octo(octos, i, j - 1)
+    energise_octo(octos, i, j + 1)
+    energise_octo(octos, i + 1, j - 1)
+    energise_octo(octos, i + 1, j)
+    energise_octo(octos, i + 1, j + 1)
